5646acd5274ee8e60a2c9f58dbd932e5da5b4063
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-11-18 22:27:11 +0000

    Add products data

---
+++ products.js
@@ -0,0 +1,82 @@
+let products = [
+  {
+    "id":1001,
+    "subject" : "Math",
+    "location" : "C223",
+    "price" : 110,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1002,
+    "subject" : "Music",
+    "location" : "H112",
+    "price" : 75,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1003,
+    "subject" : "Geography",
+    "location" : "C308",
+    "price" : 55,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1004,
+    "subject" : "English",
+    "location" : "H132",
+    "price" : 80,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1005,
+    "subject" : "Science",
+    "location" : "A111",
+    "price" : 120,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1006,
+    "subject" : "Photography",
+    "location" : "B209",
+    "price" : 85,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1007,
+    "subject" : "AI",
+    "location" : "B202",
+    "price" : 200,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1008,
+    "subject" : "History",
+    "location" : "F230",
+    "price" : 55,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1009,
+    "subject" : "Art",
+    "location" : "R106",
+    "price" : 70,
+    "spacesAvailable" : 5,
+    "image" : ""
+  },
+  {
+    "id":1010,
+    "subject" : "Biology",
+    "location" : "A203",
+    "price" : 110,
+    "spacesAvailable" : 5,
+    "image" : ""
+  }
+]
